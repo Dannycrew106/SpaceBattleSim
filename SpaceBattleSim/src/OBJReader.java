7414766b26dca05174563802013f6ac15611b94e
Danny <Danny@10.0.0.120>
2022-04-27 21:44:27 -0600

    Update from home

--- SpaceBattleSim/src/OBJReader.java
+++ SpaceBattleSim/src/OBJReader.java
@@ -1,3 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class OBJReader{
+	public static String filePath = "C:\\Users\\Danny\\git\\SpaceBattleSim\\SpaceBattleSim\\src\\";
+	//Home File path: C:\Users\Danny\git\SpaceBattleSim\SpaceBattleSim
 	
+	public static void readLine(String fileName, int line) throws FileNotFoundException{
+		System.out.println("FilePath is: " + filePath + fileName);
+		File file = new File(filePath + fileName);
+		Scanner scanner = new Scanner(file);
+		
+		scanner.useDelimiter("\\Z");
+		
+		System.out.println(scanner.next());
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		readLine("testFile.txt", 0);
+	}
 }
