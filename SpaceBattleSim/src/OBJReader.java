bd667f8058bca1b536602ec5e7eb504421e16428
Danny <Danny@10.0.0.120>
2022-04-27 22:17:41 -0600

    Updated to be able to read files

--- SpaceBattleSim/src/OBJReader.java
+++ SpaceBattleSim/src/OBJReader.java
@@ -3,20 +3,18 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class OBJReader{
-	public static String filePath = "C:\\Users\\Danny\\git\\SpaceBattleSim\\SpaceBattleSim\\src\\";
+	public static String filePath = "C:\\Users\\Danny\\git\\SpaceBattleSim\\SpaceBattleSim\\src\\objects\\";
 	//Home File path: C:\Users\Danny\git\SpaceBattleSim\SpaceBattleSim
 	
 	public static void readLine(String fileName, int line) throws FileNotFoundException{
 		System.out.println("FilePath is: " + filePath + fileName);
 		File file = new File(filePath + fileName);
 		Scanner scanner = new Scanner(file);
-		
 		scanner.useDelimiter("\\Z");
-		
 		System.out.println(scanner.next());
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		readLine("testFile.txt", 0);
+		readLine("Box v1.obj", 0);
 	}
 }
