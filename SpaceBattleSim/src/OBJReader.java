1a895797353f0259a63ef73d8ee263c6d1354857
904238007 <904238007@L829WD210015.edu.cbe.ab.ca>
2022-04-28 14:49:16 -0600

    Updated for automatic filepath finding

--- SpaceBattleSim/src/OBJReader.java
+++ SpaceBattleSim/src/OBJReader.java
@@ -1,18 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class OBJReader{
-	public static String filePath = "C:\\Users\\Danny\\git\\SpaceBattleSim\\SpaceBattleSim\\src\\objects\\";
-	//Home File path: C:\Users\Danny\git\SpaceBattleSim\SpaceBattleSim
+	public static String filePath = "C:\\Users\\904238007\\git\\SpaceBattleSim\\SpaceBattleSim\\src\\objects\\";
+	//Home File Path: C:\\Users\\Danny\\git\\SpaceBattleSim\\SpaceBattleSim\\SpaceBattleSim\\src\\objects\\
+	//School File Path: C:\\Users\\904238007\\git\\SpaceBattleSim\\SpaceBattleSim\\src\\objects\\
 	
 	public static void readLine(String fileName, int line) throws FileNotFoundException{
-		System.out.println("FilePath is: " + filePath + fileName);
-		File file = new File(filePath + fileName);
+		Path path = Paths.get(fileName);
+		System.out.println("FilePath is: " + path.toAbsolutePath().toString().replace(fileName, "\\src\\objects\\" + fileName));
+		File file = new File(path.toAbsolutePath().toString().replace(fileName, "\\src\\objects\\" + fileName));
 		Scanner scanner = new Scanner(file);
-		scanner.useDelimiter("\\Z");
+		scanner.useDelimiter("\\\\");
 		System.out.println(scanner.next());
 	}
+	
+	
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		readLine("Box v1.obj", 0);
