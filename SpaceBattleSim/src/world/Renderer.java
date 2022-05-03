122b91f40f5729808ef9a0213579534b57c7fce8
904238007 <904238007@L829WD210015.edu.cbe.ab.ca>
2022-05-03 15:02:41 -0600

    Added DrawGraphics class, should be able to draw on screen now

--- SpaceBattleSim/src/world/Renderer.java
+++ SpaceBattleSim/src/world/Renderer.java
@@ -2,18 +2,20 @@ package world;
 import javax.swing.*;
 import java.awt.*;
 
-public class Renderer extends JFrame{
+public class Renderer extends JFrame {
 	
 	private static final long serialVersionUID = 5110994671381635202L;
 	private JFrame frame = null;
-	private JPanel panel = new JPanel();
+	private JButton button = new JButton("Exit");
+	private DrawGraphics draw = new DrawGraphics();
 	
 	public void createScreen(String name, int width, int height) {
-		Font serif = new Font(Font.SERIF, Font.PLAIN, 10);
 		frame = new JFrame(name);
-		panel.setBackground(Color.BLACK);
-		frame.add(panel);
+		button.setBounds(130, 100, 100, 40);
+		frame.add(button);
+		frame.add(draw);
 		frame.setSize(width, height);
+		frame.setBackground(Color.BLACK);
 		frame.setVisible(true);
 	}
 	
